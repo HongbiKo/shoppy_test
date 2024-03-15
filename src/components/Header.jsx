014e6b4ef5a7ec5d563ab204d27c71d719bf942d
HongbiKo <rhghdql666@gmail.com>
2024-03-15 19:16:37 +0900

    making header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { MdShoppingBasket } from "react-icons/md";
+import { HiPencil } from "react-icons/hi2";
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <div>
-      header
-    </div>
+    <header>
+      <Link to='/'>
+        <MdShoppingBasket />
+        <h1>Shoppy</h1>
+      </Link>
+      <nav>
+        <Link to='/products'>Products</Link>
+        <Link to='/carts'>Carts</Link>
+        <Link to='/products/new'>
+          <HiPencil />
+        </Link>
+        <button>Login</button>
+      </nav>
+    </header>
   );
 }
 
