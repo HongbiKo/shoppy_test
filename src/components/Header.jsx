6123e4cfb7344e9a0c3c47524143940502c58296
HongbiKo <rhghdql666@gmail.com>
2024-03-28 18:51:55 +0900

    login & logout function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MdShoppingBasket } from "react-icons/md";
 import { HiPencil } from "react-icons/hi2";
 import { Link } from 'react-router-dom';
-import { login } from '../api/firbase';
+import { login, logout, onUserStateChange } from '../api/firbase';
 
 export default function Header() {
+  const [user, setUser] = useState();
+
+  const handleLogin = () => {
+    login().then(setUser);
+  };
+  const handleLogout = () => {
+    logout().then(setUser);
+  }
+
+  useEffect(()=>{
+    onUserStateChange((user) => {
+      setUser(user);
+    });
+  }, []);
+  
   return (
     <header className='flex justify-between border-b border-gray-300 p-2'>
       <Link className='flex items-center text-4xl text-brand' to='/'>
@@ -17,7 +32,8 @@ export default function Header() {
         <Link to='/products/new' className='text-2xl'>
           <HiPencil />
         </Link>
-        <button onClick={login}>Login</button>
+        {!user && <button onClick={handleLogin}>Login</button>}
+        {user && <button onClick={handleLogout}>Logout</button>}
       </nav>
     </header>
   );
