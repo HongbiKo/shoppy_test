7bb45453cbbc213f3ce55105b026c0da7504864f
HongbiKo <rhghdql666@gmail.com>
2024-04-19 15:02:24 +0900

    admin user-setting newproduct icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import { HiPencil } from "react-icons/hi2";
 import { Link } from 'react-router-dom';
 import { login, logout, onUserStateChange } from '../api/firebase';
 import User from '../components/User';
+import Button from '../components/ui/Button';
 
 export default function Header() {
   const [user, setUser] = useState();
@@ -24,12 +25,14 @@ export default function Header() {
       <nav className='flex items-center gap-4 font-semibold'>
         <Link to='/products'>Products</Link>
         <Link to='/carts'>Carts</Link>
-        <Link to='/products/new' className='text-2xl'>
-          <HiPencil />
-        </Link>
+        {user && user.isAdmin && (
+          <Link to='/products/new' className='text-2xl'>
+            <HiPencil />
+          </Link>
+        )}
         {user && <User user={user} />}
-        {!user && <button onClick={login}>Login</button>}
-        {user && <button onClick={logout}>Logout</button>}
+        {!user && <Button text={'Login'} onClick={login} />}
+        {user && <Button text={'Logout'} onClick={logout} />}
       </nav>
     </header>
   );
